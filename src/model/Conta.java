d9bbd343d7b4d0eabb11102b55a3341dc0a5c9d0
ValdenizeRibeiro <valdenizesilva8@gmail.cm>
2018-05-19 12:44:48 -0400

    class Conta e Pessoa para serem utilizadas como base. Estas classes contem o minimo com base nas informações compartilhadas dos outros grupos. A classe de transações para ser utilizada no extrato está em contrução (status: não estável). Outros grupos alterações por favor solicitem. Grupo atualize pode alterar, porém verifique o impacto causado.

---
+++ src/model/Conta.java
@@ -0,0 +1,167 @@
+package bank.model;
+
+import java.io.Serializable;
+
+/**
+*Saldo
+*senha
+*numero
+*Agencia
+*Pessoa
+*Metodos:
+*Sacar,Depositar,Emprestar,transferir,varSaldo
+*
+*
+*
+*FAZER TODA A IMPLEMENTAÇÃO DOS SETS E GET DO ATRIBUTOS E OS METODOS DETERMINADOS
+*PROXIMA AULA
+*/
+
+
+//import Cconta.Pessoa;
+/**
+ * 
+ */
+
+
+
+public class Conta implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private float Saldo;
+	private String Senha;
+	private int Numero;
+	private int Agencia;
+	private Pessoa pessoa;
+
+	public Conta( int agencia, int numero, Pessoa pessoa){
+		this.setAgencia(agencia);
+		this.setSaldo(0);
+		this.setPessoa(pessoa);
+		this.setNumero(numero);
+	}
+	//Sacar,Depositar,Emprestar,transferir,varSaldo
+	
+	public boolean sacar(int valor){
+		if (this.getSaldo() >= valor) {
+			this.setSaldo(this.getSaldo()-valor);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean depositar(int valor){
+		if (valor >= 0) {
+			this.setSaldo(this.getSaldo()+valor);
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	public boolean transferir(int valor, Conta DepositarNela){
+		if (this.Emprestar(valor)) {
+
+			if(DepositarNela.depositar(valor)){
+				this.sacar(valor);
+				return true;
+			}else{
+				//this.depositar(valor); Nao preciso fazer isso, apenas retorna false;
+				return false;
+			}				
+		}else{
+			return false;
+		}
+	}
+
+	public boolean Emprestar(int valor){
+		if (this.getSaldo() >= valor) {
+			return true;
+		}else{
+			return false;
+		}
+	}
+
+	/**
+	 * @return the pessoa
+	 */
+	public Pessoa getPessoa() {
+		return pessoa;
+	}
+
+	/**
+	 * @param pessoa the pessoa to set
+	 */
+	public void setPessoa(Pessoa pessoa) {
+		this.pessoa = pessoa;
+	}
+
+	/**
+	 * @return the agencia
+	 */
+	public int getAgencia() {
+		return Agencia;
+	}
+
+	/**
+	 * @param agencia the agencia to set
+	 */
+	public void setAgencia(int agencia) {
+		Agencia = agencia;
+	}
+
+	/**
+	 * @return the numero
+	 */
+	public int getNumero() {
+		return Numero;
+	}
+
+	/**
+	 * @param numero the numero to set
+	 */
+	public void setNumero(int numero) {
+		Numero = numero;
+	}
+
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return Senha;
+	}
+
+	/**
+	 * @param senha the senha to set
+	 */
+	public void setSenha(String senha) {
+		Senha = senha;
+	}
+
+	/**
+	 * @return the saldo
+	 */
+	public float getSaldo() {
+		return Saldo;
+	}
+
+	/**
+	 * @param saldo the saldo to set
+	 */
+	public void setSaldo(float saldo) {
+		Saldo = saldo;
+	}	
+
+	/****public void ValSaldo(){}
+	public Conta maiorSaldo(Conta teste){
+		if (this.getSaldo() > teste.getSaldo()) {
+			return this;
+		}else{
+			return teste;
+		}
+	}
+	**/
+}
---
+++ src/model/Pessoa.java
@@ -0,0 +1,117 @@
+package bank.model;
+
+import java.io.Serializable;
+
+/**
+ * 
+ */
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Pessoa implements Serializable{
+	/**
+	 * 
+	 */
+	private transient static final long serialVersionUID = 1L;
+	private String Nome;
+	private String senha;
+	private Date dtNasci;
+	private String Sexo;
+	private String Cpf;
+	private ArrayList<Conta> contas;
+	
+	public Pessoa(String nome,String senha, String cpf, Date data, String sexo){
+		this.setNome(nome);
+		this.setSenha(senha);
+		this.setCpf(cpf);
+		this.setDtNasci(data);
+		this.setContas(new ArrayList<Conta>());
+		this.setSexo(sexo);
+	}
+
+	/**
+	 * @return the nome
+	 */
+	public String getNome() {
+		return Nome;
+	}
+
+	/**
+	 * @param nome the nome to set
+	 */
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+
+	/**
+	 * @return the senha
+	 */
+	public String getSenha() {
+		return senha;
+	}
+
+	/**
+	 * @param senha the senha to set
+	 */
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	/**
+	 * @return the dtNasci
+	 */
+	public Date getDtNasci() {
+		return dtNasci;
+	}
+
+	/**
+	 * @param dtNasci the dtNasci to set
+	 */
+	public void setDtNasci(Date dtNasci) {
+		this.dtNasci = dtNasci;
+	}
+
+	/**
+	 * @return the sexo
+	 */
+	public String getSexo() {
+		return Sexo;
+	}
+
+	/**
+	 * @param sexo the sexo to set
+	 */
+	public void setSexo(String sexo) {
+		Sexo = sexo;
+	}
+
+	/**
+	 * @return the cpf
+	 */
+	public String getCpf() {
+		return Cpf;
+	}
+
+	/**
+	 * @param cpf the cpf to set
+	 */
+	public void setCpf(String cpf) {
+		Cpf = cpf;
+	}
+
+	/**
+	 * @return the contas
+	 */
+	public ArrayList<Conta> getContas() {
+		return contas;
+	}
+
+	/**
+	 * @param contas the contas to set
+	 */
+	public void setContas(ArrayList<Conta> contas) {
+		this.contas = contas;
+	}
+	
+}
