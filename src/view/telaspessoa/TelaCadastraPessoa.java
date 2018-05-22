4b7774800e31807f286b00e33d454cc93b647a14
joaoufms <gabriel_fla04@hotmail.com>
2018-05-21 22:32:35 -0400

    Feita a troca do nome da classe antiga(ExcecoesDeEntrada) para o novo nome(TratamentoDeEntradas).

--- src/view/telaspessoa/TelaCadastraPessoa.java
+++ src/view/telaspessoa/TelaCadastraPessoa.java
@@ -5,32 +5,31 @@ import view.EntradaDeDados;
 import view.ExecoesDeEntrada;
 
 public class TelaCadastraPessoa {
-	
-	public static void formularioCadastroPessoa() {
-		String cpf;
-		System.out.println();
-		System.out.println("\t\t\t*******************************************************");
-	    System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
-		System.out.println("\t\t\t*******************************************************");
-		System.out.println("\t\t\t\t\n\t\t\t\t");
-		System.out.println("\t\t\t\t****************************************");
+
+    public static void formularioCadastroPessoa() {
+        String cpf;
+        System.out.println();
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t\t\n\t\t\t\t");
+        System.out.println("\t\t\t\t****************************************");
         System.out.println("\t\t\t\t*               CADASTRO               *");
         System.out.println("\t\t\t\t****************************************");
         System.out.println("\t\t\t\t ");
-        
-        cpf = ExecoesDeEntrada.trataEntradaCpf();
-          
-        
-        if(PessoaDAO.consultaPessoaTrue(cpf) == false) {
-        	PessoaDAO.criarPessoa(ExecoesDeEntrada.trataEntradaNome(), ExecoesDeEntrada.trataEntradaDtNasc(), ExecoesDeEntrada.trataEntradaSexo(), cpf, EntradaDeDados.lerSenha());
-        	PessoaDAO.salvarPessoas();
-        	System.out.println();
-   			System.out.println("\t\t\t\t[Cadastro feito com sucesso]");
-    		System.out.println();
-        }else {
-        	System.out.println();
-   			System.out.println("\t\t\t\t[CPF não esta disponivel]");
-    		System.out.println();
+
+        cpf = TratamentodeEntradas.trataEntradaCpf();
+
+        if (PessoaDAO.consultaPessoaTrue(cpf) == false) {
+            PessoaDAO.criarPessoa(TratamentodeEntradas.trataEntradaNome(), TratamentodeEntradas.trataEntradaDtNasc(), TratamentodeEntradas.trataEntradaSexo(), cpf, EntradaDeDados.lerSenha());
+            PessoaDAO.salvarPessoas();
+            System.out.println();
+            System.out.println("\t\t\t\t[Cadastro feito com sucesso]");
+            System.out.println();
+        } else {
+            System.out.println();
+            System.out.println("\t\t\t\t[CPF não esta disponivel]");
+            System.out.println();
         }
-	}
+    }
 }
