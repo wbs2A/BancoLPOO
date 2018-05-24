2ec6ca2eed53793360c2fd3b5c6254ce11c89b1e
wbs2A <wbs0616@gmail.com>
2018-05-24 11:37:01 -0400

    Correção dos erros de sintaxe

---
+++ Banco/src/controller/DAO.java
@@ -0,0 +1,41 @@
+package controller;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+public abstract class DAO{
+	
+/*
+ * Metodo para deserializar
+ * 
+ */
+public  Object deserializar(String path) throws Exception
+{
+	FileInputStream inFile = new FileInputStream(path);
+	Object o;
+	try (ObjectInputStream d = new ObjectInputStream(inFile))
+	{
+		o = d.readObject();
+	}
+	return o;
+}
+
+
+
+/**
+ * 
+ * Metodo para serializar
+ * 
+ */
+public  void serializar(String path, Object obj) throws Exception
+{
+	FileOutputStream outFile = new FileOutputStream(path);
+	try (ObjectOutputStream s = new ObjectOutputStream(outFile))
+	{
+		s.writeObject(obj);
+	}
+
+}
+}
---
+++ Banco/src/controller/PessoaDAO.java
@@ -0,0 +1,102 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.Date;
+import model.Pessoa;
+
+public class PessoaDAO extends DAO{
+    
+    public static void salvarPessoas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static void carregarPessoas() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    public static Object getPessoa() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    Pessoa get(String cpf, String senha) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+
+    private static ArrayList<Pessoa> arrayPessoa  = new ArrayList<Pessoa>();
+
+
+     /* Metodo instancia objeto do tipo pessoa (criando uma pessoa) e adiciona o objeto pessoa no array de pessoas
+     *@author José Sandonas
+     *@param String nome que representa o nome da pessoa
+     *@param String senha que representa a senha de cadastro
+     *@param String cpf que representa o cadastro
+     *@param Date data que representa a data de nascimento
+     *@param String sexo genero seja ele qual for
+     *@return pessoa
+     */
+    public static Object create(String nome,String senha, String cpf, Date data, String sexo, String telefone, String email){
+        Pessoa pessoa = new Pessoa(nome, senha, cpf, data, sexo, telefone, email);
+        arrayPessoa.add(pessoa);
+        return pessoa;
+    }
+
+    /* Metodo que realiza a busca de uma pessoa recebendo como parametro o seu CPF
+    *@author José Sandonas
+    *@return pessoa especificada ou nulo caso nao encontrada  
+    */
+    public static Pessoa read(String cpf){
+        for(Pessoa pessoa:arrayPessoa){
+            if(pessoa.getCpf().equals(cpf)){
+                return pessoa;
+            }
+        }
+        return null;
+    }
+    /* Metodo que realiza a alteração das informacoes de uma pessoa recebendo como parametro o numero que especifica 
+    qual dado que sera alterado, pessoa do tipo Pessoa, obejto info que seria a informação que sera alterada 
+    *@author José Sandonas
+    *@return pessoa especificada ou nulo caso nao encontrada  
+    */
+    public void update(int operacao, Pessoa pessoa, Object info){
+        switch (operacao) {
+            case 1:
+                pessoa.setNome((String)info);
+                break;
+            case 2:
+                pessoa.setSenha((String)info);
+                break;
+            case 3:
+                pessoa.setDtNasci((Date) info);
+                break;
+            case 4:
+                pessoa.setSexo((String) info);
+                break;
+            case 5:
+                pessoa.setTelefone((String) info);
+                break;
+            case 6:
+                pessoa.setEmail((String) info);
+                break;
+        }
+    
+    }
+    /*Metodo que executa  a remocao interna que remove a pessoa contida dentro do arraypessoa
+    *@param pessoa
+    *@author José Sandonas
+    */
+    private static void delete(Pessoa pessoa){
+        arrayPessoa.remove(pessoa);
+    }
+
+    /*Metodo que executa  a remocao externa que recebe o cpf e remove a pessoa
+    *@param String cpf
+    *@author José Sandonas
+    */
+    public static void delete(String cpf){
+        Pessoa p = read(cpf);
+        delete(p);
+    }
+
+
+}
