a9b758f60fc6b0640aafd95a87f8cd939d6aa945
João Gabriel <gabriel_fla04@hotmail.com>
2018-05-24 17:39:17 -0400

    Adicionado os comentários da classe, e de seu método.

--- src/view/comum/MenuInicial.java
+++ src/view/comum/MenuInicial.java
@@ -1,20 +1,39 @@
 package view.comum;
 
+/**
+ * Classe responsavel por disponibilizar as opcoes do Enum MenuInicial
+ *
+ * @author Michael Douglas
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Adler Cavalcante
+ */
 public enum MenuInicial {
-	FAZERLOGIN(1), FAZERCADASTRO(2), SAIR(0);
-	int opcao;
+    FAZERLOGIN(1), FAZERCADASTRO(2), SAIR(0);
+    int opcao;
 
-	MenuInicial(int op) {
-	this.opcao = op;
-	}
+    /**
+     *
+     * @param op
+     */
+    MenuInicial(int op) {
+        this.opcao = op;
+    }
 
-	public static MenuInicial menuOpcao(int opcao) {
-		if (opcao == 1)
-			return MenuInicial.FAZERLOGIN;
-		else if (opcao == 2)
-			return MenuInicial.FAZERCADASTRO;
-		else if (opcao == 0)
-			return MenuInicial.SAIR;
-		return null;
-	}
+    /**
+     * Metodo para retorno da opcao escolhida
+     *
+     * @param opcao int - Valor da opcao escolhida
+     * @return enum opcao - Opccao com nome correspondente
+     */
+    public static MenuInicial menuOpcao(int opcao) {
+        if (opcao == 1) {
+            return MenuInicial.FAZERLOGIN;
+        } else if (opcao == 2) {
+            return MenuInicial.FAZERCADASTRO;
+        } else if (opcao == 0) {
+            return MenuInicial.SAIR;
+        }
+        return null;
+    }
 }
