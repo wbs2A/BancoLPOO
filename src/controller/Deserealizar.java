0d056b6f4f34a5b86e3896df512f0488913a308e
harrisonmk <harrison.mitchell@hotmail.com>
2018-05-24 11:31:33 -0400

    Classe para fazer a Deserialização

---
+++ src/controller/Deserealizar.java
@@ -0,0 +1,20 @@
+package controle;
+
+import java.io.FileInputStream;
+import java.io.ObjectInputStream;
+
+public class Deserealizar {
+
+    public  Object deserializar(String path) throws Exception {
+    FileInputStream inFile = new FileInputStream(path);
+    Object o;
+        try (ObjectInputStream d = new ObjectInputStream(inFile)) {
+            o = d.readObject();
+        }
+    return o;
+    } 
+
+
+
+
+}
