3f7374e9d64a33c2bc9dcb952db1e76f10613682
João Gabriel <gabriel_fla04@hotmail.com>
2018-05-24 11:24:34 -0400

    Adicionado os comentários da Classe, e de seu método.

--- src/view/telasconta/MenuConta.java
+++ src/view/telasconta/MenuConta.java
@@ -1,28 +1,47 @@
 package view.telasconta;
 
+/**
+ * Classe responsavel por disponibilizar as opcoes do Enum MenuConta
+ *
+ * @author Michael Douglas
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Adler Cavalcante
+ */
 public enum MenuConta {
-	REALIZARTRANSACAO(1), EXIBIREXTRATO(2), CADASTRARCONTA(3), 
-	EXCLUIRCONTA(4), CONSULTARCONTA(5), SAIR(0);
-	
-	int opcao;
-	
-	MenuConta(int op) {
-		this.opcao = op;
-	}
-	
-	public static MenuConta menuOpcao(int opcao) {
-		if(opcao == 1)
-			return MenuConta.REALIZARTRANSACAO;
-		else if(opcao == 2)
-			return MenuConta.EXIBIREXTRATO;
-		else if(opcao == 3)
-			return MenuConta.CADASTRARCONTA;
-		else if(opcao == 4)
-			return MenuConta.EXCLUIRCONTA;
-		else if(opcao == 5)
-			return MenuConta.CONSULTARCONTA;
-		else if(opcao == 0)
-			return MenuConta.SAIR;
-		return null;
-	}
+    REALIZARTRANSACAO(1), EXIBIREXTRATO(2), CADASTRARCONTA(3),
+    EXCLUIRCONTA(4), CONSULTARCONTA(5), SAIR(0);
+
+    int opcao;
+
+    /**
+     *
+     * @param op
+     */
+    MenuConta(int op) {
+        this.opcao = op;
+    }
+
+    /**
+     * Metodo para retorno da opcao escolhida
+     *
+     * @param opcao int - Valor da opcao escolhida
+     * @return enum opcao - Opcao com o nome correspondente
+     */
+    public static MenuConta menuOpcao(int opcao) {
+        if (opcao == 1) {
+            return MenuConta.REALIZARTRANSACAO;
+        } else if (opcao == 2) {
+            return MenuConta.EXIBIREXTRATO;
+        } else if (opcao == 3) {
+            return MenuConta.CADASTRARCONTA;
+        } else if (opcao == 4) {
+            return MenuConta.EXCLUIRCONTA;
+        } else if (opcao == 5) {
+            return MenuConta.CONSULTARCONTA;
+        } else if (opcao == 0) {
+            return MenuConta.SAIR;
+        }
+        return null;
+    }
 }
