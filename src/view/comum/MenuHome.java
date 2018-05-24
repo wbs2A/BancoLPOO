e8f13fb6c031528c89f6c64c01f8651706eda930
João Gabriel <gabriel_fla04@hotmail.com>
2018-05-24 17:38:19 -0400

    Adicionado os comentários da classe, e de seu método.

--- src/view/comum/MenuHome.java
+++ src/view/comum/MenuHome.java
@@ -1,20 +1,39 @@
 package view.comum;
 
+/**
+ * Classe responsavel por disponibilizar as opcoes do Enum MenuInicial
+ *
+ * @author Michael Douglas
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Adler Cavalcante
+ */
 public enum MenuHome {
-	GERENCIARCONTA(1), GERENCIARPESSOA(2), SAIR(0);
-	int opcao;
-	
-	MenuHome(int op) {
-	this.opcao = op;
-	}
-	
-	public static MenuHome menuOpcao(int opcao) {
-		if(opcao == 1)
-			return MenuHome.GERENCIARCONTA;
-		else if(opcao == 2)
-			return MenuHome.GERENCIARPESSOA;
-		else if(opcao == 0)
-			return MenuHome.SAIR;
-		return null;
-	}
+    GERENCIARCONTA(1), GERENCIARPESSOA(2), SAIR(0);
+    int opcao;
+
+    /**
+     *
+     * @param op
+     */
+    MenuHome(int op) {
+        this.opcao = op;
+    }
+
+    /**
+     * Metodo para retorno da opcao escolhida
+     *
+     * @param opcao int - Valor da opcao escolhida
+     * @return enum opcao - Opcao com o nome correspondente
+     */
+    public static MenuHome menuOpcao(int opcao) {
+        if (opcao == 1) {
+            return MenuHome.GERENCIARCONTA;
+        } else if (opcao == 2) {
+            return MenuHome.GERENCIARPESSOA;
+        } else if (opcao == 0) {
+            return MenuHome.SAIR;
+        }
+        return null;
+    }
 }
