51ed89e297c7f3c2d629424b18e942efaf0e19ed
João Gabriel <gabriel_fla04@hotmail.com>
2018-05-24 11:37:23 -0400

    Adicionado os comentários da classe, e de seu método. Além disso foi adicionado na classe o atributo opcao no qual é chamado no construtor da classe.

--- src/view/telasconta/MenuDefineContaPadrao.java
+++ src/view/telasconta/MenuDefineContaPadrao.java
@@ -1,16 +1,37 @@
 package view.telasconta;
 
+/**
+ * Classe responsavel por disponibilizar as opcoes do Enum MenuDefineContaPadrao
+ *
+ * @author Michael Douglas
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Adler Cavalcante
+ */
 public enum MenuDefineContaPadrao {
-	SIM(1), NAO(2);
-	
-	private MenuDefineContaPadrao(int op) {
-	}
-	
-	public static MenuDefineContaPadrao menuOpcao(int opcao) {
-		if(opcao == 1)
-			return MenuDefineContaPadrao.SIM;
-		else if(opcao == 2)
-			return MenuDefineContaPadrao.NAO;
-		return null;
-	}
+    SIM(1), NAO(2);
+    int opcao;
+
+    /**
+     *
+     * @param op
+     */
+    private MenuDefineContaPadrao(int op) {
+        this.opcao = op;
+    }
+
+    /**
+     * Metodo para retorno da opcao escolhida
+     *
+     * @param opcao int - Valor da opcao escolhida
+     * @return enum opcao - Opcao com o nome correspondente
+     */
+    public static MenuDefineContaPadrao menuOpcao(int opcao) {
+        if (opcao == 1) {
+            return MenuDefineContaPadrao.SIM;
+        } else if (opcao == 2) {
+            return MenuDefineContaPadrao.NAO;
+        }
+        return null;
+    }
 }
