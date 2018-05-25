1f87afc2ec2c5dcfb48081e7e5a0e947ad7d4e22
DOUGLASMD1 <36670769+DOUGLASMD1@users.noreply.github.com>
2018-05-25 14:14:16 -0400

    Removido um dos métodos duplicados menuGerenciaPessoas() ocasionado pelo HEAD

--- src/view/telaspessoa/TelaGerenciaPessoa.java
+++ src/view/telaspessoa/TelaGerenciaPessoa.java
@@ -13,8 +13,6 @@ import view.TratamentodeEntradas;
  */
 
 public class TelaGerenciaPessoa {
-<<<<<<< HEAD
-
 
 /**
  * Metodo para impressao da tela para gerenciar dados de cadastro e receber a opcao que o usuario escolher 
@@ -66,52 +64,3 @@ public class TelaGerenciaPessoa {
 		}while(!sair);
 	}
 }
-=======
-
-    public static void menuGerenciaPessoas() {
-        int opcao;
-        boolean sair = false;
-
-        do {
-            try {
-                new ClearConsole();
-                System.out.println();
-                System.out.println("\t\t\t*******************************************************");
-                System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
-                System.out.println("\t\t\t*******************************************************");
-                System.out.println("\t\t\t\t\n\t\t\t\t");
-                System.out.println("\t \t\t\t**************************************");
-                System.out.println("\t\t\t\t*    GERENCIAR DADOS DE CADASTRO      *");
-                System.out.println("\t \t\t\t**************************************");
-                System.out.println("\t\t\t\t\n\t\t\t\t");
-                System.out.println("\t\t\t\t**************************************");
-				System.out.println("\t\t\t\t* "+MenuPessoa.ATUALIZARDADOS.opcao+". Atualizar Dados de Cadastro *");
-				System.out.println("\t\t\t\t**************************************");
-				System.out.println("\t\t\t\t* "+MenuPessoa.EXLUCIRPESSOA.opcao". Excluir Conta *");
-				System.out.println("\t\t\t\t**************************************");
-				System.out.println("\t\t\t\t*   "+MenuPessoa.SAIR+". Voltar      *");
-                System.out.println("\t\t\t\t**************************************");
-                opcao = TratamentodeEntradas.trataEntradaOpcao();
-
-                switch (MenuPessoa.menuOpcao(opcao)) {
-                    case ATUALIZARDADOS:
-                        TelaAtualizaDadosPessoa.formularioAtualizaCadastro();
-                        break;
-
-                    case EXLUCIRPESSOA:
-                        TelaExcluiPessoa.menuExcluirPessoa();
-                        break;
-
-                    case SAIR:
-                        sair = true;
-                        break;
-                }
-            } catch (Exception e) {
-                System.out.println();
-                System.out.println("\t\t\t\tOpcao Invalida!");
-                System.out.println();
-            }
-        } while (!sair);
-    }
-}
->>>>>>> 84556cf0b193c18f1b64b2077c9955e9108d91d1
