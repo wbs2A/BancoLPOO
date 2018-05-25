4a70c712812d879ea714f9edfe9bd535833d8a1b
DOUGLASMD1 <36670769+DOUGLASMD1@users.noreply.github.com>
2018-05-25 14:12:42 -0400

    Update TelaExcluiPessoa.java
    Foi removido um dos métodos duplicados void menuExcluirPessoa() ocasionado pelo HEAD

--- src/view/telaspessoa/TelaExcluiPessoa.java
+++ src/view/telaspessoa/TelaExcluiPessoa.java
@@ -20,7 +20,6 @@ import view.TratamentodeEntradas;
 
 
 public class TelaExcluiPessoa {
-<<<<<<< HEAD
 /**
  * Metodo para impressao da tela excluirconta e receber a opcao escolhida pelo usuario
  */
@@ -88,71 +87,4 @@ public class TelaExcluiPessoa {
 			System.out.println();
 		}   
 	}
-=======
-
-    public static void menuExcluirPessoa() {
-        int opcao;
-        boolean sair = false;
-        Pessoa pessoa;
-
-        System.out.println();
-        System.out.println("\t\t\t\t          Confirme sua Identidade                ");
-        pessoa = Login.validaLogin(TratamentodeEntradas.trataEntradaCpf(), EntradaDeDados.lerSenha());
-
-        if (pessoa != null) {
-            do {
-                try {
-                    new ClearConsole();
-                    System.out.println();
-                    System.out.println("\t\t\t*******************************************************");
-                    System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
-                    System.out.println("\t\t\t*******************************************************");
-                    System.out.println("\t\t\t\t\n\t\t\t\t");
-                    System.out.println("\t \t\t\t**************************************");
-                    System.out.println("\t\t\t\t*            EXCLUIR CONTA           *");
-                    System.out.println("\t \t\t\t**************************************");
-                    System.out.println();
-                    System.out.println("\t\t\t\t  Deseja realmente excluir sua Conta?  ");
-                    System.out.println("\t\t\tAo excluir, seus dados serão perdidos permanentemente");
-                    System.out.println("\t\t\t  Todas as suas Contas Bancarias também serão perdidas");
-                    System.out.println();
-                    System.out.println("\t \t\t\t**************************************");
-        			System.out.println("\t\t\t\t*   ("+MenuExcluirConta.NAO.opcao+")Não                *");
-                    System.out.println("\t\t\t\t**************************************");
-                    System.out.println("\t\t\t\t*   ("+MenuExcluirConta.SIM.opcao+")Sim                *");
-                    System.out.println("\t\t\t\t**************************************");
-                    System.out.println("\t\t\t\t\n\t\t\t\t");
-                    opcao = TratamentodeEntradas.trataEntradaOpcao();
-
-                    switch (MenuExcluirConta.menuConfirma(opcao)) {
-                        case NAO:
-                            sair = true;
-                            break;
-
-                        case SIM:
-                            PessoaDAO.removerPessoa(pessoa);
-                            PessoaDAO.salvarPessoas();
-                            ContaDAO.salvarContas();
-                            System.out.println();
-                            System.out.println("\t\t\t\t[Conta Excluida com sucesso]");
-                            System.out.println();
-                            System.exit(0);
-                            sair = true;
-                            break;
-
-                    }
-
-                } catch (Exception e) {
-                    System.out.println();
-                    System.out.println("\t\t\t\tOpcao Invalida!");
-                    System.out.println();
-                }
-            } while (!sair);
-        } else {
-            System.out.println();
-            System.out.println("\t\t\t\t[Usuario e/ou Senha Incorreto(s)]");
-            System.out.println();
-        }
-    }
->>>>>>> 84556cf0b193c18f1b64b2077c9955e9108d91d1
-}
+}	
