3e4c96d13bd9f4f1dfa4b73eab0e8982844c40a6
DOUGLASMD1 <michael.silva@aluno.ufms.br>
2018-05-27 21:07:41 -0400

    Adicionado um bloco try catch para a chamada do metodo validaLogin da classe Controller, alguns imports não utilizados foram removidos, também foi removido a  variavel (pessoa).

--- src/view/telasconta/TelaCadastraConta.java
+++ src/view/telasconta/TelaCadastraConta.java
@@ -1,9 +1,8 @@
 package view.telasconta;
 
 import controller.ContaDAO;
-import controller.Login;
+import controller.Controller;
 import model.Conta;
-import model.Pessoa;
 import view.EntradaDeDados;
 import view.TratamentodeEntradas;
 
@@ -29,7 +28,6 @@ public class TelaCadastraConta {
 	 */
 	public static void formularioCadastroConta() {
 		float saldo;
-		Pessoa pessoa;
 		Conta conta;
 
 		System.out.println();
@@ -41,27 +39,28 @@ public class TelaCadastraConta {
 		System.out.println("\t\t\t\t*      CADASTRO DE CONTA BANCARIA      *");
 		System.out.println("\t\t\t\t****************************************");
 		System.out.println("\t\t\t\t");
-
 		System.out.println();
 		System.out.println("\t\t\t\t        Confirme sua Identidade         ");
-		pessoa = Login.validaLogin(TratamentodeEntradas.trataEntradaCpf(), EntradaDeDados.lerSenha());
 
-		if (pessoa != null) {
-			saldo = TratamentodeEntradas.trataEntradaSaldoConta();
-			if (saldo > 0) {
-				conta = ContaDAO.criarConta(saldo, pessoa);
-				System.out.println();
-				System.out.println("\t\t\t\t[Conta Criada com Sucesso]");
-				System.out.println();
-				System.out.println(conta);
-				System.out.println("\t\t\t\tSenha: " + conta.getSenha());
-				System.out.println();
-			} else {
-				System.out.println();
-				System.out.println("\t\t\t\t[Saldo Invalido]");
-				System.out.println();
+		try {
+			if (Controller.validaLogin(TratamentodeEntradas.trataEntradaCpf(), EntradaDeDados.lerSenha())) {
+				saldo = TratamentodeEntradas.trataEntradaSaldoConta();
+				if (saldo > 0) {
+					conta = (Conta) ContaDAO.create(EntradaDeDados.lerSenhaConta(), saldo, Controller.getPessoa());
+					System.out.println();
+					System.out.println("\t\t\t\t[Conta Criada com Sucesso]");
+					System.out.println();
+					System.out.println(conta);
+					System.out.println("\t\t\t\tSenha: " + conta.getSenha());
+					System.out.println();
+				} else {
+					System.out.println();
+					System.out.println("\t\t\t\t[Saldo Invalido]");
+					System.out.println();
+				}
 			}
-		} else {
+
+		} catch (Exception e) {
 			System.out.println();
 			System.out.println("\t\t\t\t[Usuario e/ou senha incorreto(s)]");
 			System.out.println();
