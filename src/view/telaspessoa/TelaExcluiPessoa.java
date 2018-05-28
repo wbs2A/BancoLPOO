55c2adb8d02b202018c362014e953cdda03451b5
DOUGLASMD1 <michael.silva@aluno.ufms.br>
2018-05-27 20:51:10 -0400

    Adicionado um bloco try catch para a chamada do metodo validaLogin da classe Controller, alguns imports não utilizados foram removidos.

--- src/view/telaspessoa/TelaExcluiPessoa.java
+++ src/view/telaspessoa/TelaExcluiPessoa.java
@@ -1,16 +1,15 @@
 package view.telaspessoa;
 
 import controller.ContaDAO;
-import controller.Login;
+import controller.Controller;
 import controller.PessoaDAO;
-import model.Pessoa;
 import view.ClearConsole;
 import view.EntradaDeDados;
 import view.TratamentodeEntradas;
 
-
 /**
- * Classe responsavel por disponibilizar a tela que ira oferecer as opcaoes de exclusao de conta 
+ * Classe responsavel por disponibilizar a tela que ira oferecer as opcaoes de
+ * exclusao de conta
  *
  * @author Michael Douglas
  * @author Joao Gabriel
@@ -18,52 +17,51 @@ import view.TratamentodeEntradas;
  * @author Adler Cavalcante
  */
 
-
 public class TelaExcluiPessoa {
-/**
- * Metodo para impressao da tela excluirconta e receber a opcao escolhida pelo usuario
- */
+	/**
+	 * Metodo para impressao da tela excluirconta e receber a opcao escolhida pelo
+	 * usuario
+	 */
 	public static void menuExcluirPessoa() {
 		int opcao;
 		boolean sair = false;
-		Pessoa pessoa;
-		
+
 		System.out.println();
 		System.out.println("\t\t\t\t          Confirme sua Identidade                ");
-		pessoa = Login.validaLogin(TratamentodeEntradas.trataEntradaCpf(), EntradaDeDados.lerSenha());
-		
-		if(pessoa != null) {
-			do {
-				try {
-					new ClearConsole();
-					System.out.println();
-					System.out.println("\t\t\t*******************************************************");
-					System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
-					System.out.println("\t\t\t*******************************************************");
-					System.out.println("\t\t\t\t\n\t\t\t\t");
-					System.out.println("\t \t\t\t**************************************");
-					System.out.println("\t\t\t\t*            EXCLUIR CONTA           *");
-					System.out.println("\t \t\t\t**************************************");
-					System.out.println();
-					System.out.println("\t\t\t\t  Deseja realmente excluir sua Conta?  ");
-					System.out.println("\t\t\tAo excluir, seus dados serão perdidos permanentemente");
-					System.out.println("\t\t\t  Todas as suas Contas Bancarias também serão perdidas");
-					System.out.println();
-					System.out.println("\t \t\t\t**************************************");
-					System.out.println("\t\t\t\t*    ("+MenuExcluirConta.NAO.opcao+")Não                *");
-					System.out.println("\t\t\t\t**************************************");
-					System.out.println("\t\t\t\t*    ("+MenuExcluirConta.SIM.opcao+")Sim                *");
-					System.out.println("\t\t\t\t**************************************");
-					System.out.println("\t\t\t\t\n\t\t\t\t");
-					opcao = TratamentodeEntradas.trataEntradaOpcao();
-					
-					switch(MenuExcluirConta.menuConfirma(opcao)) {
+
+		try {
+			if (Controller.validaLogin(TratamentodeEntradas.trataEntradaCpf(), EntradaDeDados.lerSenha())) {
+				do {
+					try {
+						new ClearConsole();
+						System.out.println();
+						System.out.println("\t\t\t*******************************************************");
+						System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
+						System.out.println("\t\t\t*******************************************************");
+						System.out.println("\t\t\t\t\n\t\t\t\t");
+						System.out.println("\t \t\t\t**************************************");
+						System.out.println("\t\t\t\t*            EXCLUIR CONTA           *");
+						System.out.println("\t \t\t\t**************************************");
+						System.out.println();
+						System.out.println("\t\t\t\t  Deseja realmente excluir sua Conta?  ");
+						System.out.println("\t\t\tAo excluir, seus dados serão perdidos permanentemente");
+						System.out.println("\t\t\t  Todas as suas Contas Bancarias também serão perdidas");
+						System.out.println();
+						System.out.println("\t \t\t\t**************************************");
+						System.out.println("\t\t\t\t*    (" + MenuExcluirConta.NAO.opcao + ")Não                *");
+						System.out.println("\t\t\t\t**************************************");
+						System.out.println("\t\t\t\t*    (" + MenuExcluirConta.SIM.opcao + ")Sim                *");
+						System.out.println("\t\t\t\t**************************************");
+						System.out.println("\t\t\t\t\n\t\t\t\t");
+						opcao = TratamentodeEntradas.trataEntradaOpcao();
+
+						switch (MenuExcluirConta.menuConfirma(opcao)) {
 						case NAO:
 							sair = true;
 							break;
-						
+
 						case SIM:
-							PessoaDAO.removerPessoa(pessoa);
+							PessoaDAO.delete(Controller.getPessoa());
 							PessoaDAO.salvarPessoas();
 							ContaDAO.salvarContas();
 							System.out.println();
@@ -72,19 +70,20 @@ public class TelaExcluiPessoa {
 							System.exit(0);
 							sair = true;
 							break;
-							
+
+						}
+
+					} catch (Exception e) {
+						System.out.println();
+						System.out.println("\t\t\t\tOpcao Invalida!");
+						System.out.println();
 					}
-			        
-				}catch(Exception e) {
-					System.out.println();
-					System.out.println("\t\t\t\tOpcao Invalida!");
-					System.out.println();
-				}
-			}while(!sair);
-		}else{
+				} while (!sair);
+			}
+		} catch (Exception e) {
 			System.out.println();
 			System.out.println("\t\t\t\t[Usuario e/ou Senha Incorreto(s)]");
 			System.out.println();
-		}   
+		}
 	}
-}	
+}
