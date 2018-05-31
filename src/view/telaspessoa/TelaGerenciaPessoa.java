f3e9eea845006d0fb7d00413114e26a5604301a8
Adler Moraes <adlercmoraes98@gmail.com>
2018-05-31 15:13:12 -0400

    Modificações telaspessoa
    Adicionada uma nova opcao na tela do método menuGerenciaPessoas para dizer que existe a opcao de visualizar os dados pessoais de uma pessoa e um novo case foi criado para acessar a Tela TelaConsultaDadosPessoa.

--- src/view/telaspessoa/TelaGerenciaPessoa.java
+++ src/view/telaspessoa/TelaGerenciaPessoa.java
@@ -4,7 +4,8 @@ import view.ClearConsole;
 import view.TratamentodeEntradas;
 
 /**
- * Classe responsavel por disponibilizar a tela que ira oferecer as opcoes atulizar dados e excluir conta
+ * Classe responsavel por disponibilizar a tela que ira oferecer as opcoes
+ * atulizar dados e excluir conta
  *
  * @author Michael Douglas
  * @author Joao Gabriel
@@ -14,14 +15,15 @@ import view.TratamentodeEntradas;
 
 public class TelaGerenciaPessoa {
 
-/**
- * Metodo para impressao da tela para gerenciar dados de cadastro e receber a opcao que o usuario escolher 
- */
+	/**
+	 * Metodo para impressao da tela para gerenciar dados de cadastro e receber a
+	 * opcao que o usuario escolher
+	 */
 
-	public static void menuGerenciaPessoas(){
+	public static void menuGerenciaPessoas() {
 		int opcao;
 		boolean sair = false;
-		
+
 		do {
 			try {
 				new ClearConsole();
@@ -35,32 +37,40 @@ public class TelaGerenciaPessoa {
 				System.out.println("\t \t\t\t**************************************");
 				System.out.println("\t\t\t\t\n\t\t\t\t");
 				System.out.println("\t\t\t\t**************************************");
-				System.out.println("\t\t\t\t*   "+MenuPessoa.ATUALIZARDADOS.opcao+". Atualizar Dados de Cadastro *");
+				System.out
+						.println("\t\t\t\t*   " + MenuPessoa.ATUALIZARDADOS.opcao + ". Atualizar Dados de Cadastro *");
 				System.out.println("\t\t\t\t**************************************");
-				System.out.println("\t\t\t\t*   "+MenuPessoa.EXLUCIRPESSOA.opcao+". Excluir Conta *");
+				System.out.println("\t\t\t\t*   " + MenuPessoa.EXLUCIRPESSOA.opcao + ". Excluir Conta *");
 				System.out.println("\t\t\t\t**************************************");
-				System.out.println("\t\t\t\t*   "+MenuPessoa.SAIR.opcao+". Voltar      *");
+				System.out.println(
+						"\t\t\t\t*   " + MenuPessoa.VERDADOSPESSOA.opcao + ". Mostrar dados Cadastrados Pessoa *");
 				System.out.println("\t\t\t\t**************************************");
-		        opcao = TratamentodeEntradas.trataEntradaOpcao();
-		        
-		        switch(MenuPessoa.menuOpcao(opcao)){
-		        	case ATUALIZARDADOS:
-		        		TelaAtualizaDadosPessoa.formularioAtualizaCadastro();
-		        		break;
-		        	
-		        	case EXLUCIRPESSOA:
-		        		TelaExcluiPessoa.menuExcluirPessoa();
-		        		break;
-		        	
-		        	case SAIR:
-		        		sair = true;
-		        		break;
-		        }
-			}catch(Exception e) {
+				System.out.println("\t\t\t\t*   " + MenuPessoa.SAIR.opcao + ". Voltar      *");
+				System.out.println("\t\t\t\t**************************************");
+				opcao = TratamentodeEntradas.trataEntradaOpcao();
+
+				switch (MenuPessoa.menuOpcao(opcao)) {
+				case ATUALIZARDADOS:
+					TelaAtualizaDadosPessoa.formularioAtualizaCadastro();
+					break;
+
+				case EXLUCIRPESSOA:
+					TelaExcluiPessoa.menuExcluirPessoa();
+					break;
+
+				case VERDADOSPESSOA:
+					TelaConsultaDadosPessoa.menuConsultaDadosPessoa();
+					break;
+
+				case SAIR:
+					sair = true;
+					break;
+				}
+			} catch (Exception e) {
 				System.out.println();
 				System.out.println("\t\t\t\tOpcao Invalida!");
 				System.out.println();
 			}
-		}while(!sair);
+		} while (!sair);
 	}
 }
