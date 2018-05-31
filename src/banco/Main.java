bfd3e335bea438e27a9200b3c2eb7b54d2ee0b05
NathalyAmaral <nathalyamaral07@gmail.com>
2018-05-31 14:59:56 -0300

    Adicionada a chamada da serialização e deserialização de PessoaDAO e ContaDAO na classe Main

--- src/banco/Main.java
+++ src/banco/Main.java
@@ -1,10 +1,18 @@
 package banco;
 
+import controller.ContaDAO;
+import controller.PessoaDAO;
 import view.comum.TelaInicial;
 
 public class Main{
 
     public static void main(String[] args){
+    	PessoaDAO.carregarPessoas();
+    	ContaDAO.carregarContas();
+    	
         TelaInicial.telaInicial();
+    
+        PessoaDAO.salvarPessoas();
+        ContaDAO.salvarContas();
     }
 }
