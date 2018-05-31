e598abaab210eb38c31ea258342469a31dfd0d85
joaoufms <gabriel_fla04@hotmail.com>
2018-05-31 15:24:52 -0400

    Criação de um novo enum que será utiliado na tela Extrato Conta, contendo duas opções, Extrato da Conta Padrao e Extrato de Outra Conta.

---
+++ src/view/telasconta/MenuExtratoConta.java
@@ -0,0 +1,40 @@
+package view.telasconta;
+
+/**
+ * Classe responsável por disponiblizar as opcoes do Enum MenuExtratoConta
+ *
+ * @author Joao Gabriel
+ * @author Michael Douglas
+ * @author Joao Victor
+ * @author Adler Moraes
+ */
+public enum MenuExtratoConta {
+    EXTRATOCONTAPADRAO(1), EXTRATOOUTRACONTA(2), SAIR(0);
+    int opcao;
+
+    /**
+     *
+     * @param op
+     */
+    MenuExtratoConta(int op) {
+        this.opcao = op;
+
+    }
+
+    /**
+     * Metodo para retorno da opcao escolhida
+     *
+     * @param opcao int - Valor da opcao escolhida
+     * @return enum opcao - Opcao com o nome correspondente
+     */
+    public static MenuExtratoConta menuOpcao(int opcao) {
+        if (opcao == 1) {
+            return MenuExtratoConta.EXTRATOCONTAPADRAO;
+        } else if (opcao == 2) {
+            return MenuExtratoConta.EXTRATOOUTRACONTA;
+        } else if (opcao == 0) {
+            return MenuExtratoConta.SAIR;
+        }
+        return null;
+    }
+}
