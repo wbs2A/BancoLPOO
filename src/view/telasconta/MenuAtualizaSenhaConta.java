b6f4fa8839cb4cda98df7830aa056517c6b461dc
Adler Moraes <adlercmoraes98@gmail.com>
2018-05-31 02:34:59 -0400

    Modificações telasconta
    Comentários foram adicionados na classe MenuAtualizaSenhaConta, explicando o funcionamento da classe e de cada um de seus métodos.

--- src/view/telasconta/MenuAtualizaSenhaConta.java
+++ src/view/telasconta/MenuAtualizaSenhaConta.java
@@ -1,14 +1,34 @@
 package view.telasconta;
 
+/**
+ * Classe responsavel por disponibilizar as opcoes do Enum
+ * MenuAtualizaSenhaConta
+ *
+ * @author Michael Douglas
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Adler Cavalcante
+ */
 public enum MenuAtualizaSenha {
 	ATUALIZARSENHACONTAPADRAO(1), ATUALIZARSENHAOUTRACONTA(2), SAIR(0);
 
 	int opcao;
 
+	/**
+	 *
+	 * @param op
+	 */
 	MenuAtualizaSenha(int op) {
 		this.opcao = op;
 	}
 
+	/**
+	 * Metodo para retorno da opcao escolhida
+	 *
+	 * @param opcao
+	 *            int - Valor da opcao escolhida
+	 * @return enum opcao - Opcao com o nome correspondente
+	 */
 	public static MenuAtualizaSenha menuOpcao(int opcao) {
 		if (opcao == 1) {
 			return MenuAtualizaSenha.ATUALIZARSENHACONTAPADRAO;
