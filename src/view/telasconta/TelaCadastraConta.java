e7e3c1e2b889f5b6470755e792e820f3f0ad53a9
DOUGLASMD1 <michael.silva@aluno.ufms.br>
2018-06-01 15:12:12 -0400

    Adicionada a impressão do saldo inicial da conta criada.

--- src/view/telasconta/TelaCadastraConta.java
+++ src/view/telasconta/TelaCadastraConta.java
@@ -51,6 +51,7 @@ public class TelaCadastraConta {
 				System.out.println();
 				System.out.println(conta);
 				System.out.println("\t\t\t\tSenha: " + conta.getSenha());
+				System.out.println("\t\t\t\tSaldo: " + conta.getSaldo());
 				System.out.println();
 				//ContaDAO.salvarContas();
 				System.out.println("\t\t\t\tPressione Enter para continuar...");
