4c93d9e8f7278cacc5674cf1ea2cb30cb6b10b47
joaoufms <gabriel_fla04@hotmail.com>
2018-06-02 17:15:14 -0400

    Nova tela que mostra como cada opção da tela Gerencia Pessoa funciona. Possui um método que cria imprime as explicações de cada opção.

---
+++ src/view/telaspessoa/TelaAjudaPessoa.java
@@ -0,0 +1,44 @@
+package view.telaspessoa;
+
+/**
+ * Classe responsavel por disponiblizar uma tela de ajuda ao usuario, que mostra
+ * o que cada opcao da tela Gerenciar Dados de Cadastro faz faz.
+ *
+ * @author Joao Gabriel
+ * @author Michael Douglas
+ * @author Adler Cavalcante
+ * @author Joao Victor
+ */
+public class TelaAjudaPessoa {
+
+    /**
+     * O metodo AjudaPessoa e cria uma tela onde cada opcao do menu Gerenciar
+     * Dados de Cadastro e descrita, mostrando ao usuario de que maneira ela
+     * funciona e quais os dados que devem ser inseridos.
+     */
+    public static void AjudaPessoa() {
+        System.out.println();
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*\t           CPAN BANCO CENTER                  *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t\t\n\t\t\t\t");
+        System.out.println("\t\t\t\t**************************************");
+        System.out.println("\t\t\t\t*                 AJUDA          *");
+        System.out.println("\t\t\t\t**************************************");
+        System.out.println("\t\t\t\t\n\t\t\t\t");
+        System.out.println("\t\t\t\t* Opcao 1 - Atualizar dados de cadastro : Opcao que faz a atualizacao de seus dados cadastrais. Voce pode escolher para atualizar seu Nome,*");
+        System.out.println("\t\t\t\t*                                         CPF,Data de Nascimento,Sexo,Senha de Login,Celular e E-mail. Quando uma opcao e escolhida, e mos-*");
+        System.out.println("\t\t\t\t*                                         trado o dado atual e pede-se para digitar o novo dado.*");
+        System.out.println("\t\t\t\t******************************************************************************************************************************************************");
+        System.out.println("\t\t\t\t* Opcao 2 - Excluir Pessoa: Opcao que exclui os seus cadastro do banco, com todo os seus dados excluidos, assim como todas as suas contas cri-*");
+        System.out.println("\t\t\t\t*                           adas. Antes de excluir e preciso confirmar se realmente deseja excluir seu cadastro, caso escolha sim basta confi- *");
+        System.out.println("\t\t\t\t*                           rmar sua senha.*");
+        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
+        System.out.println("\t\t\t\t* Opcao 3 - Mostrar dados Cadastrados Pessoa: Opcao que mostra todos os dados que foram cadastrados. Para ver seus dados, basta confirmar sua*");
+        System.out.println("\t\t\t\t*                            senha, e todos os seus dados serao exibidos.*");
+        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
+        System.out.println("\t\t\t\t* Opcao 4 - Sair: Opcao que sai do menu Gerenciar Dados de Cadastro.*");
+        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
+
+    }
+}
