2671c345cd2c2e80187cab1b3a1a697b1bb2ba89
DOUGLASMD1 <michael.silva@aluno.ufms.br>
2018-06-03 18:20:30 -0400

    Ajustes de icones.

--- src/view/telaspessoa/TelaAjudaPessoa.java
+++ src/view/telaspessoa/TelaAjudaPessoa.java
@@ -26,19 +26,28 @@ public class TelaAjudaPessoa {
         System.out.println("\t\t\t\t*                 AJUDA          *");
         System.out.println("\t\t\t\t**************************************");
         System.out.println("\t\t\t\t\n\t\t\t\t");
-        System.out.println("\t\t\t\t* Opcao 1 - Atualizar dados de cadastro : Opcao que faz a atualizacao de seus dados cadastrais. Voce pode escolher para atualizar seu Nome,*");
-        System.out.println("\t\t\t\t*                                         CPF,Data de Nascimento,Sexo,Senha de Login,Celular e E-mail. Quando uma opcao e escolhida, e mos-*");
-        System.out.println("\t\t\t\t*                                         trado o dado atual e pede-se para digitar o novo dado.*");
-        System.out.println("\t\t\t\t******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 2 - Excluir Pessoa: Opcao que exclui os seus cadastro do banco, com todo os seus dados excluidos, assim como todas as suas contas cri-*");
-        System.out.println("\t\t\t\t*                           adas. Antes de excluir e preciso confirmar se realmente deseja excluir seu cadastro, caso escolha sim basta confi- *");
-        System.out.println("\t\t\t\t*                           rmar sua senha.*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 3 - Mostrar dados Cadastrados Pessoa: Opcao que mostra todos os dados que foram cadastrados. Para ver seus dados, basta confirmar sua*");
-        System.out.println("\t\t\t\t*                            senha, e todos os seus dados serao exibidos.*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 4 - Sair: Opcao que sai do menu Gerenciar Dados de Cadastro.*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
-
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 1-Atualizar dados de cadastro : Opcao que faz a*"
+        		+ "\n\t\t\t*atualizacao de seus dados cadastrais. Voce pode      *"
+        		+ "\n\t\t\t*escolher para atualizar seu Nome, CPF,Data de        *"
+        		+ "\n\t\t\t*Nascimento, Sexo, Senha de Login,Celular e E-mail.   *"
+        		+ "\n\t\t\t*Quando uma opcao e escolhida, e mostrado o dado atual*"
+        		+ "\n\t\t\t*e pede-se para digitar o novo dado.                  *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 2-Excluir Pessoa: Opcao que exclui o seu       *"
+        		+ "\n\t\t\t*cadastro do banco, com todo os seus dados excluidos, *"
+        		+ "\n\t\t\t*assim como todas as suas contas bancarias criadas.   *"
+        		+ "\n\t\t\t*Antes de excluir e preciso confirmar se realmente    *"
+        		+ "\n\t\t\t*deseja excluir seu cadastro, caso escolha sim basta  *"
+        		+ "\n\t\t\t*confirmar sua senha.                                 *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 3-Mostrar dados Cadastrados Pessoa: Opcao que  *"
+        		+ "\n\t\t\t*mostra todos os dados que foram cadastrados. Para ver* "
+        		+ "\n\t\t\t*seus dados, basta confirmar sua senha, e todos os    *"
+        		+ "\n\t\t\t*seus dados serao exibidos.                           *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 4-Sair: Opcao que sai do menu Gerenciar Dados  *"
+        		+ "\n\t\t\t*de Cadastro.                                         *");
+        System.out.println("\t\t\t*******************************************************");
     }
 }
