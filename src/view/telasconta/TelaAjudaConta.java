f752d89f872ce47536ece0bf9784e95636b98196
DOUGLASMD1 <michael.silva@aluno.ufms.br>
2018-06-03 18:53:40 -0400

    Ajustes de icone.

--- src/view/telasconta/TelaAjudaConta.java
+++ src/view/telasconta/TelaAjudaConta.java
@@ -26,33 +26,64 @@ public class TelaAjudaConta {
         System.out.println("\t\t\t\t*                 AJUDA          *");
         System.out.println("\t \t\t\t**************************************");
         System.out.println("\t\t\t\t\n\t\t\t\t");
-        System.out.println("\t\t\t\t* Opcao 1 - Transacao : O banco oferece tres tipos de transacao, sendo elas Saque,Deposito e Transferencia. Para realizar uma transacao e*");
-        System.out.println("\t\t\t\t*                       necessario ter pelo menos ter uma Conta Padrao ou Outra Conta. Caso escolha Conta Padrao, basta inserir o valor da transacaoo ");
-        System.out.println("\t\t\t\t*                       e em seguida inserir a senha.Caso escolha Outra Conta, sera necessario inserir o numero e a conta da senha, e em ");
-        System.out.println("\t\t\t\t*                       seguida inserir o valor da transacaoo. ");
-        System.out.println("\t \t\t\t******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 2 - Exibir Extrato de uma Conta: O banco oferece extrato para dois tipos de conta, sendo Extrato da Conta Padrao e o Extrato de Outra Conta. Caso escolha*");
-        System.out.println("\t\t\t\t*                                        Conta Padrao sera mostrado o extrato completo da conta. Caso escolha Outra Conta, e necessario inserir o Numero*");
-        System.out.println("\t\t\t\t*                                        e a Senha da Conta se as credenciais estiverem certas, o extrato da conta sera exibido.*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 3 - Cadastrar Conta: Para o cadastro de uma Conta, insira seu CPF e sua Senha, caso as credenciais estejam certas, o banco ira gerar a Agencia e*");
-        System.out.println("\t\t\t\t*                            e Numero da conta,em seguida insira uma senha que deseja para a conta.*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 4 - Excluir Conta: Para a exclusao de uma conta, insira o numero e a senha da conta que deseja excluir, em seguida confirme sua identidade*");
-        System.out.println("\t\t\t\t*                          inserindo seu CPF e sua Senha, caso as credencias estejam certas e confirme que deseja excluir, a conta sera removida do banco. Lembran-*");
-        System.out.println("\t\t\t\t*                          do que e necessario ter a conta cadastrada no banco para excluir.*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 5 - Definir Conta Padrao: Uma Conta Padrao serve para fazer manipulacoes com uma conta de maneira mais facil, seja para ver o extrato, fazer*");
-        System.out.println("\t\t\t\t*                                 transacoes,etc. Em todas essas manipulacoes, com a Conta Padrao, basta inserir a senha da conta.*");
-        System.out.println("\t\t\t\t******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 6 - Atualiza Senha da Conta: Para atualizar a senha de uma conta e necessario escolher o tipo da conta. Caso escolha Conta Padrao, o banco*");
-        System.out.println("\t\t\t\t*                                    mostrara a Senha Atual e pedira a nova Senha que deseja cadastrar. Caso escolha Outra Conta, e necessario inserir o numero e a senha da*");
-        System.out.println("\t\t\t\t*                                    conta, caso as credenciais esetejam corretas, o banco mostrara a Senha Atual e pedira a nova Senha que deseja cadastrar.*");
-        System.out.println("\t\t\t\t******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 7 - Ajuda: Opcao que mostra o que cada opcao no Menu de Gerenciar Conta faz.*");
-        System.out.println("\t\t\t\t******************************************************************************************************************************************************");
-        System.out.println("\t\t\t\t* Opcao 8 - Sair: Sai do Menu de Gerenciar Conta e volta para o Menu Home*");
-        System.out.println("\t\t\t\t*******************************************************************************************************************************************************");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 1-Transacao : O banco oferece tres tipos de    *"
+        		+ "\n\t\t\t*transacao, sendo elas Saque,Deposito e Transferencia.*"
+        		+ "\n\t\t\t*Para realizar uma transacao e necessario ter pelo    *"
+        		+ "\n\t\t\t*menos ter uma Conta Padrao ou Outra Conta. Caso      *"
+        		+ "\n\t\t\t*escolha Conta Padrao, basta inserir o valor da       *"
+        		+ "\n\t\t\t*transacaoo e em seguida inserir a senha.Caso escolha *"
+        		+ "\n\t\t\t*Outra Conta, sera necessario inserir o numero e a    *"
+        		+ "\n\t\t\t*conta da senha, e em seguida inserir o valor da      *"
+        		+ "\n\t\t\t*transacaoo.                                          *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 2-Exibir Extrato de uma Conta: O banco oferece *"
+        		+ "\n\t\t\t*extrato para dois tipos de conta, sendo Extrato da   *"
+        		+ "\n\t\t\t*Conta Padrao e o Extrato de Outra Conta. Caso escolha*"
+        		+ "\n\t\t\t*Conta Padrao sera mostrado o extrato completo da     *"
+        		+ "\n\t\t\t*conta. Caso escolha Outra Conta, e necessario inserir*"
+        		+ "\n\t\t\t*o Numero e a Senha da Conta se as credenciais        *"
+        		+ "\n\t\t\t*estiverem certas, o extrato da conta sera exibido.   *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 3-Cadastrar Conta: Para o cadastro de uma Conta*"
+        		+ "\n\t\t\t*insira seu CPF e sua Senha, caso as credenciais      *"
+        		+ "\n\t\t\t*estejam certas, o banco o Numero da conta, em seguida* "
+        		+ "\n\t\t\t*insira uma senha que deseja para a conta.            *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 4-Excluir Conta: Para a exclusao de uma conta, *"
+        		+ "\n\t\t\t*insira o numero e a senha da conta que deseja excluir*"
+        		+ "\n\t\t\t*em seguida confirme sua identidade inserindo seu CPF *"
+        		+ "\n\t\t\t*e sua Senha, caso as credencias estejam certas e     *"
+        		+ "\n\t\t\t*confirme que deseja excluir, a conta sera removida do* "
+        		+ "\n\t\t\t*banco. Lembrando que e necessario ter a conta        *"
+        		+ "\n\t\t\t*cadastrada no banco para excluir.                    *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 5-Consultar Conta: A consulta de conta lista   *"
+        		+ "\n\t\t\t*todas as contas cadastradas em seu cpf. Sao exibidos *"
+        		+ "\n\t\t\t*dados como nome, agencia, numero e saldo da conta.   *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 6-Definir Conta Padrao: Uma Conta Padrao serve *"
+        		+ "\n\t\t\t*para fazer manipulacoes com uma conta de maneira mais*"
+        		+ "\n\t\t\t*facil, seja para ver o extrato, fazer transacoes,etc.*"
+        		+ "\n\t\t\t*Em todas essas manipulacoes, com a Conta Padrao,     *"
+        		+ "\n\t\t\t*basta inserir a senha da conta.                      *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 7-Atualiza Senha da Conta: Para atualizar a    *"
+        		+ "\n\t\t\t*senha de uma conta e necessario escolher o tipo da   *"
+        		+ "\n\t\t\t*conta. Caso escolha Conta Padrao, o banco mostrara a *"
+        		+ "\n\t\t\t*Senha Atual e pedira a nova Senha que deseja         *"
+        		+ "\n\t\t\t*cadastrar. Caso escolha Outra Conta, e necessario    *"
+        		+ "\n\t\t\t*inserir o numero e a senha da conta, caso as         *"
+        		+ "\n\t\t\t*credenciais esetejam corretas, o banco mostrara a    *"
+        		+ "\n\t\t\t*Senha Atual e pedira a nova Senha que deseja         *"
+        		+ "\n\t\t\t*cadastrar.                                           *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 8-Ajuda: Opcao que mostra o que cada opcao no  *"
+        		+ "\n\t\t\t*Menu de Gerenciar Conta faz.                         *");
+        System.out.println("\t\t\t*******************************************************");
+        System.out.println("\t\t\t*Opcao 0-Sair: Sai do Menu de Gerenciar Conta         *"
+        		+ "\n\t\t\t*e volta para o Menu Home.                            *");
+        System.out.println("\t\t\t*******************************************************");
 
     }
 }
