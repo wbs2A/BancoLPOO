ebdad22b2cae5f3241dea1329b06a8333e4e1152
ValdenizeRibeiro <valdenizesilva8@gmail.cm>
2018-06-04 11:23:28 -0400

    Corrigindo indentação e formatação da class Main. Adicionando o asteristico no comentario para gerar javadoc.

--- src/banco/Main.java
+++ src/banco/Main.java
@@ -3,20 +3,21 @@ package banco;
 import controller.ContaDAO;
 import controller.PessoaDAO;
 import view.comum.TelaInicial;
-/*
+
+/**
  * Classe que representa a inicializacao da aplicacao e carregamento e descarregamento de objetos.
  * @author: Nathaly.
  * @author: Wesley B.
  */
-public class Main{
+public class Main {
 
-    public static void main(String[] args){
-    	PessoaDAO.carregarPessoas();
-    	ContaDAO.carregarContas();
-    	
-        TelaInicial.telaInicial();
-    
-        PessoaDAO.salvarPessoas();
-        ContaDAO.salvarContas();
-    }
+	public static void main(String[] args) {
+		PessoaDAO.carregarPessoas();
+		ContaDAO.carregarContas();
+
+		TelaInicial.telaInicial();
+
+		PessoaDAO.salvarPessoas();
+		ContaDAO.salvarContas();
+	}
 }
