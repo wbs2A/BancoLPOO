f1bb2faac5706de83149df639a475d1d7caaac7f
ValdenizeRibeiro <valdenizesilva8@gmail.cm>
2018-06-04 11:58:56 -0400

    Adicionando o asteristico no comentario para gerar javadoc na class DAO.

--- src/controller/DAO.java
+++ src/controller/DAO.java
@@ -10,76 +10,83 @@ import java.io.ObjectOutputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/*
- * Classe que representa os processo de serializacao e deserializacao de objetos.
+/**
+ * Classe que representa os processo de serializacao e deserializacao de
+ * objetos.
+ * 
  * @author: Nathaly.
  * @author: Wesley B.
  */
 
-abstract class DAO<T>{
-    
-    /*
-    * Metodo para Deserializar um objeto dado seu tipo como parametro.
-    *  Ele carrega o objeto encontrado no arquivo do FileInputStream e
-    *  e registra as excecoes no Logger da classe.
-    * @author: Wesley B.
-    * @param: Nome do arquivo
-    * @return: Objeto a ser convertido para a classe correta.
-    */
-    public static Object carregar(String type){
- 
-	        try{
-	        	FileInputStream fis = new FileInputStream(type);	
-	        	ObjectInputStream ois = new ObjectInputStream(fis);
-	        	Object obj = (Object)ois.readObject();
-	            ois.close();
-	            return obj;
-	        } catch (FileNotFoundException ex) {
-	            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
-	            return null;
-	        } catch (ClassNotFoundException | IOException ex) {
-	            Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
-	            return null;
-	        }
+abstract class DAO<T> {
 
-        }
-  
+	/**
+	 * Metodo para Deserializar um objeto dado seu tipo como parametro. Ele
+	 * carrega o objeto encontrado no arquivo do FileInputStream e e registra as
+	 * excecoes no Logger da classe.
+	 * 
+	 * @author: Wesley B.
+	 * @param: Nome
+	 *             do arquivo
+	 * @return: Objeto a ser convertido para a classe correta.
+	 */
+	public static Object carregar(String type) {
 
-    /*
-    * Metodo para Serializar um objeto dado como parametro.
-    *  Ele descarrega o objeto no arquivo criado com o
-    *  FileInputStream e registra as excecoes no Logger da classe.
-    * @author: Wesley B.
-    * @param: Nome do arquivo e Objeto a ser serializado.
-    * @return: Booleano informando o resultado da operacao.
-    */
+		try {
+			FileInputStream fis = new FileInputStream(type);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			Object obj = (Object) ois.readObject();
+			ois.close();
+			return obj;
+		} catch (FileNotFoundException ex) {
+			Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+			return null;
+		} catch (ClassNotFoundException | IOException ex) {
+			Logger.getLogger(DAO.class.getName()).log(Level.SEVERE, null, ex);
+			return null;
+		}
 
-    public static boolean descarregar(String type,Object obj){
-        try{
-        	FileOutputStream fos = new FileOutputStream(type);	
-        	ObjectOutputStream oos = new ObjectOutputStream(fos);
-            oos.writeObject(obj);
-            oos.close();
-            return true;
-            
-        } catch (FileNotFoundException ex) {
-        	ex.printStackTrace();
-            return false;
-        } catch(IOException ex){
-        	ex.printStackTrace();
-        	return false;
-        }
-    }
-    
-    /*
-    * Metodo utilizado para converter o objeto lido do arquivo
-    *  num dos tipos utilizados no sistema.
-    * @author: Wesley B.
-    * @param: Objeto a alterar-se com o cast.
-    * @return: Objeto no tipo solicitado.
-    */
-    @SuppressWarnings("unchecked")
-    public static <T> T castTo(Object obj) {
-        return (T) obj;
-    }
+	}
+
+	/**
+	 * Metodo para Serializar um objeto dado como parametro. Ele descarrega o
+	 * objeto no arquivo criado com o FileInputStream e registra as excecoes no
+	 * Logger da classe.
+	 * 
+	 * @author: Wesley B.
+	 * @param: Nome
+	 *             do arquivo e Objeto a ser serializado.
+	 * @return: Booleano informando o resultado da operacao.
+	 */
+
+	public static boolean descarregar(String type, Object obj) {
+		try {
+			FileOutputStream fos = new FileOutputStream(type);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(obj);
+			oos.close();
+			return true;
+
+		} catch (FileNotFoundException ex) {
+			ex.printStackTrace();
+			return false;
+		} catch (IOException ex) {
+			ex.printStackTrace();
+			return false;
+		}
+	}
+
+	/**
+	 * Metodo utilizado para converter o objeto lido do arquivo num dos tipos
+	 * utilizados no sistema.
+	 * 
+	 * @author: Wesley B.
+	 * @param: Objeto
+	 *             a alterar-se com o cast.
+	 * @return: Objeto no tipo solicitado.
+	 */
+	@SuppressWarnings("unchecked")
+	public static <T> T castTo(Object obj) {
+		return (T) obj;
+	}
 }
