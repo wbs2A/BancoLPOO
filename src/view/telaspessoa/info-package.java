2eb895769c55445dcea0327122372986bee36379
João Gabriel <gabriel_fla04@hotmail.com>
2018-06-07 11:20:14 -0400

    Um info-package foi adicionado dentro de telaspessoa para descrever o pacote.

---
+++ src/view/telaspessoa/info-package.java
@@ -0,0 +1,12 @@
+/**
+ * O pacote telaspessoa contem as seguintes classes: MenuAtualizaDados, MenuExcluirConta,
+ * MenuPessoa, TelaAjudaPessoa, TelaAtualizaDadosPessoa, TelaCadastraPessoa,TelaConsultaDadosPessoa,TelaExcluirPessoa,TelaGerenciaPessoa.
+ * Sendo elas as telas que gerenciam as possiveis acoes relacionadas a uma pessoa.
+ *
+ * @author Adler Cavalcante
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Michael Douglas
+ * @since 1.0
+ */
+package telaspessoa;
