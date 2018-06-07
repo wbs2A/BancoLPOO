4d0b4dde9da77cd9559e2fbcd724dec8a6658943
João Gabriel <gabriel_fla04@hotmail.com>
2018-06-07 11:17:29 -0400

    Esta classe teve seus comentários corrigidos e sua identação ajustada.

--- src/view/telaspessoa/TelaAjudaPessoa.java
+++ src/view/telaspessoa/TelaAjudaPessoa.java
@@ -1,13 +1,14 @@
 package view.telaspessoa;
 
 /**
- * Classe responsavel por disponiblizar uma tela de ajuda ao usuario, que mostra
- * o que cada opcao da tela Gerenciar Dados de Cadastro faz faz.
+ * A Classe TelaAjudaPessoa e responsavel por disponiblizar uma tela de ajuda ao
+ * usuario, que mostra o que cada opcao da tela Gerenciar Dados de Cadastro faz
+ * faz.
  *
- * @author Joao Gabriel
- * @author Michael Douglas
  * @author Adler Cavalcante
+ * @author Joao Gabriel
  * @author Joao Victor
+ * @author Michael Douglas
  */
 public class TelaAjudaPessoa {
 
@@ -28,26 +29,26 @@ public class TelaAjudaPessoa {
         System.out.println("\t\t\t\t\n\t\t\t\t");
         System.out.println("\t\t\t*******************************************************");
         System.out.println("\t\t\t*Opcao 1-Atualizar dados de cadastro : Opcao que faz a*"
-        		+ "\n\t\t\t*atualizacao de seus dados cadastrais. Voce pode      *"
-        		+ "\n\t\t\t*escolher para atualizar seu Nome, CPF,Data de        *"
-        		+ "\n\t\t\t*Nascimento, Sexo, Senha de Login,Celular e E-mail.   *"
-        		+ "\n\t\t\t*Quando uma opcao e escolhida, e mostrado o dado atual*"
-        		+ "\n\t\t\t*e pede-se para digitar o novo dado.                  *");
+                + "\n\t\t\t*atualizacao de seus dados cadastrais. Voce pode      *"
+                + "\n\t\t\t*escolher para atualizar seu Nome, CPF,Data de        *"
+                + "\n\t\t\t*Nascimento, Sexo, Senha de Login,Celular e E-mail.   *"
+                + "\n\t\t\t*Quando uma opcao e escolhida, e mostrado o dado atual*"
+                + "\n\t\t\t*e pede-se para digitar o novo dado.                  *");
         System.out.println("\t\t\t*******************************************************");
         System.out.println("\t\t\t*Opcao 2-Excluir Pessoa: Opcao que exclui o seu       *"
-        		+ "\n\t\t\t*cadastro do banco, com todo os seus dados excluidos, *"
-        		+ "\n\t\t\t*assim como todas as suas contas bancarias criadas.   *"
-        		+ "\n\t\t\t*Antes de excluir e preciso confirmar se realmente    *"
-        		+ "\n\t\t\t*deseja excluir seu cadastro, caso escolha sim basta  *"
-        		+ "\n\t\t\t*confirmar sua senha.                                 *");
+                + "\n\t\t\t*cadastro do banco, com todo os seus dados excluidos, *"
+                + "\n\t\t\t*assim como todas as suas contas bancarias criadas.   *"
+                + "\n\t\t\t*Antes de excluir e preciso confirmar se realmente    *"
+                + "\n\t\t\t*deseja excluir seu cadastro, caso escolha sim basta  *"
+                + "\n\t\t\t*confirmar sua senha.                                 *");
         System.out.println("\t\t\t*******************************************************");
         System.out.println("\t\t\t*Opcao 3-Mostrar dados Cadastrados Pessoa: Opcao que  *"
-        		+ "\n\t\t\t*mostra todos os dados que foram cadastrados. Para ver* "
-        		+ "\n\t\t\t*seus dados, basta confirmar sua senha, e todos os    *"
-        		+ "\n\t\t\t*seus dados serao exibidos.                           *");
+                + "\n\t\t\t*mostra todos os dados que foram cadastrados. Para ver* "
+                + "\n\t\t\t*seus dados, basta confirmar sua senha, e todos os    *"
+                + "\n\t\t\t*seus dados serao exibidos.                           *");
         System.out.println("\t\t\t*******************************************************");
         System.out.println("\t\t\t*Opcao 4-Sair: Opcao que sai do menu Gerenciar Dados  *"
-        		+ "\n\t\t\t*de Cadastro.                                         *");
+                + "\n\t\t\t*de Cadastro.                                         *");
         System.out.println("\t\t\t*******************************************************");
     }
 }
