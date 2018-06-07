8d733322ac24ae341437db96b6f1f373a0d72084
Adler Moraes <adlercmoraes98@gmail.com>
2018-06-07 10:42:20 -0400

    Modificações comum
    Um info-package foi adicionado dentro da comum para descrever o pacote.

---
+++ src/view/comum/info-package.java
@@ -0,0 +1,14 @@
+
+/**
+ * O pacote comum contem as seguintes classes: MenuHome, MenuInicial,
+ * TelaAjudaInicial, TelaGerenciamentoContaPessoa, TelaInicial e TelaLogin.
+ * Sendo elas as telas iniciais da aplicacao.
+ * 
+ * @author Adler Cavalcante
+ * @author Joao Gabriel
+ * @author Joao Victor
+ * @author Michael Douglas
+ * @since 1.0 
+ */
+
+package comum;
