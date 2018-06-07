85e7f6fbb500fc730057a6c5a994a710fcc0dda8
AllisonVinicius <allisonvinicus63@gmail.com>
2018-06-07 08:56:06 -0300

    Adicionado descrição (info-package) pacote view

---
+++ src/view/info-package.java
@@ -0,0 +1,17 @@
+
+
+/**
+ * Este pacote contem as classe view da aplicacao da aplicacao.
+ * As classes EntradaDeDados, TratamentoEntradas sao responsaveis por
+ * receber os valores de entrada da leitura do teclado e tratar essas entradas 
+ * caso nao estejam nos padroes estabelecidos.
+ * 
+ *  @author Adler_Moraes
+ * @author Michael Douglas
+ * @author Joao Gabriel
+ * @author Joao Victor
+ *@since 1.0
+ */
+ 
+ 
+ package view;
